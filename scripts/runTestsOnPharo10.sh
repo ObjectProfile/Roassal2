cb9f7608021c042a70075e83763a91a427759535
JanBliznicenko <Jan.Bliznicenko@gmail.com>
2022-07-22 13:00:11 +0200

    Added Pharo10 CI workflow

---
+++ scripts/runTestsOnPharo10.sh
@@ -0,0 +1,19 @@
+curl -L https://get.pharo.org/64/100+vm | bash
+./pharo --headless Pharo.image ./scripts/runTests.st
+
+FILE=/tmp/result.txt
+if [ ! -f "$FILE" ]; then
+    echo "ERROR: $FILE does not exists!"
+    exit 1
+fi
+
+
+cat $FILE
+
+if grep -q ERROR "$FILE"; then
+		echo "SOME ERRORS!"
+		exit 1
+else
+		echo "ALL TEST PASSED"
+		exit 0
+fi
